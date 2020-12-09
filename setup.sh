301dbdf11ac99a11ebacfbd345aed61e6cb15d65
Gene Garcia <eugarci@microsoft.com>
2020-12-09 13:02:51 -0500

    fix setup to be able to setup in a diff location

--- setup.sh
+++ setup.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 # Provisions and configures the OpenEduAnalytics base architecture, as well as the Contoso package.
-if [ $# -ne 1 ]; then
+if [ $# -ne 1 ] && [ $# -ne 2 ]; then
     echo "This setup script will install the Open Edu Analytics base architecture along with the ContosoISD package and test data into a newly created resource group."
-    echo "Invoke this script like this:  setup.sh <OrgId>"
-    echo "where OrgId is an id for the env (eg, CISD3)"
+    echo "Invoke this script like this:  setup.sh <orgId>"
+    echo "where orgId is the id for your organization (eg, ContosoISD3)."
+    echo "By default, the Azure resources will be provisioned in  the East US location."
+    echo "If you want to have the resources provisioned in an alternate location, invoke the script like this: setup.sh <orgId> <location>"
+    echo "where orgId is the id for your organization (eg, ContosoISD3), and location is the abbreviation of the desired location (eg, eastus, westus, northeurope)."
     exit 1
 fi
 
 org_id=$1
 org_id_lowercase=${org_id,,}
+location=$2
+location=${location:-eastus}
+
 resource_group="EduAnalytics${org_id}"
 synapse_workspace="syeduanalytics${org_id_lowercase}"
 
@@ -17,7 +23,7 @@ synapse_workspace="syeduanalytics${org_id_lowercase}"
 oea_path=$(dirname $(realpath $0))
 
 # setup the base architecture
-$oea_path/setup_base_architecture.sh $org_id
+$oea_path/setup_base_architecture.sh $org_id $location
 # install the ContosoISD package
 $oea_path/packages/ContosoISD/setup.sh $org_id
 
