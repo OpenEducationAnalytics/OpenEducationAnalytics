01debb795ea3af34d5dd31bf9540c9f961f0253e
genegc <eugarci@microsoft.com>
2021-08-25 12:32:45 -0400

    update

--- setup_base_architecture.sh
+++ setup_base_architecture.sh
@@ -73,7 +73,7 @@ az synapse workspace firewall-rule create --name allowAll --workspace-name $OEA_
   --start-ip-address 0.0.0.0 --end-ip-address 255.255.255.255
 
 echo "--> Creating spark pool."
-az synapse spark pool create --name spark3p0sm --workspace-name $OEA_SYNAPSE --resource-group $OEA_RESOURCE_GROUP \
+az synapse spark pool create --name spark3p1sm --workspace-name $OEA_SYNAPSE --resource-group $OEA_RESOURCE_GROUP \
   --spark-version 3.1 --node-count 3 --node-size Small --min-node-count 3 --max-node-count 10 \
   --enable-auto-scale true --delay 15 --enable-auto-pause true \
   --no-wait
