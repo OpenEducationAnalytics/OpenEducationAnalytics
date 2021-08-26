97dadc38375653d3e59906d9cecc8b462ea4e36a
genegc <eugarci@microsoft.com>
2021-08-26 10:12:32 -0400

    add validation msg to console output

--- setup.sh
+++ setup.sh
@@ -41,7 +41,9 @@ subscription_id=$(az account show --query id -o tsv)
 # The length is constrained by the fact that the synapse workspace name must be <= 24 characters, and our naming convention requires that it start with "syn-oea-".
 if [[ ${#org_id} -gt 16 || ! $org_id =~ ^[a-zA-Z0-9]+$ ]]; then
   echo "Invalid suffix: $org_id"
+  echo "Invalid suffix: $org_id" 1>&3
   echo "The chosen suffix must be less than 12 characters, and must contain only letters and numbers."
+  echo "The chosen suffix must be less than 12 characters, and must contain only letters and numbers." 1>&3
   exit 1
 fi
 
@@ -49,7 +51,9 @@ fi
 roles=$(az role assignment list --subscription $subscription_id --query [].roleDefinitionName -o tsv)
 if [[ ! " ${roles[@]} " =~ "Owner" ]]; then
   echo "You do not have the role assignment of Owner on this subscription."
+  echo "You do not have the role assignment of Owner on this subscription." 1>&3
   echo "For more info, click here -> https://github.com/microsoft/OpenEduAnalytics/wiki/Setup-Tips#error-must-have-role-assignment-of-owner-on-subscription"
+  echo "For more info, click here -> https://github.com/microsoft/OpenEduAnalytics/wiki/Setup-Tips#error-must-have-role-assignment-of-owner-on-subscription" 1>&3
   exit 1
 fi
 
