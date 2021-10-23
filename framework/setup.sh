e4800c00a68174329e88e4184d28aded81953f4f
genegc <eugarci@microsoft.com>
2021-10-23 18:17:50 -0400

    update framework setup

--- framework/setup.sh
+++ framework/setup.sh
@@ -14,17 +14,18 @@ synapse_workspace=$1
 this_file_path=$(dirname $(realpath $0))
 
 echo "--> Setting up the OEA framework assets."
-#az synapse dataset create --workspace-name $synapse_workspace --name data_lake_binary --file @$this_file_path/dataset/data_lake_binary.json
-#az synapse dataset create --workspace-name $synapse_workspace --name data_lake_csv --file @$this_file_path/dataset/data_lake_csv.json
+# install integration assets
+eval "az synapse linked-service create --workspace-name $synapse_workspace --name LS_ADLS_OEA --file @$this_file_path/linkedService/LS_ADLS_OEA.json"
+eval "az synapse linked-service create --workspace-name $synapse_workspace --name LS_HTTP --file @$this_file_path/linkedService/LS_HTTP.json"
 eval "az synapse dataset create --workspace-name $synapse_workspace --name DS_ADLS_binary --file @$this_file_path/dataset/DS_ADLS_binary.json"
 eval "az synapse dataset create --workspace-name $synapse_workspace --name DS_HTTP_binary --file @$this_file_path/dataset/DS_HTTP_binary.json"
-eval "az synapse notebook import --workspace-name $synapse_workspace --name OEA_py --file @$this_file_path/notebook/OEA_py.ipynb --only-show-errors"
-eval "az synapse notebook import --workspace-name $synapse_workspace --name DataGen_py --file @$this_file_path/notebook/DataGen_py.ipynb --only-show-errors"
-eval "az synapse notebook import --workspace-name $synapse_workspace --name data_generation_example --file @$this_file_path/notebook/data_generation_example.ipynb --only-show-errors"
-eval "az synapse linked-service create --workspace-name $synapse_workspace --name LS_HTTP --file @$this_file_path/linkedService/LS_HTTP.json"
 eval "az synapse pipeline create --workspace-name $synapse_workspace --name OEA_ex1_data_ingestion --file @$this_file_path/pipeline/OEA_ex1_data_ingestion.json"
 eval "az synapse pipeline create --workspace-name $synapse_workspace --name OEA_ex2_data_ingestion --file @$this_file_path/pipeline/OEA_ex2_data_ingestion.json"
 eval "az synapse pipeline create --workspace-name $synapse_workspace --name OEA_ex3_data_ingestion --file @$this_file_path/pipeline/OEA_ex3_data_ingestion.json"
+# install notebooks
+eval "az synapse notebook import --workspace-name $synapse_workspace --name OEA_py --file @$this_file_path/notebook/OEA_py.ipynb --only-show-errors"
+eval "az synapse notebook import --workspace-name $synapse_workspace --name DataGen_py --file @$this_file_path/notebook/DataGen_py.ipynb --only-show-errors"
+eval "az synapse notebook import --workspace-name $synapse_workspace --name data_generation_example --file @$this_file_path/notebook/data_generation_example.ipynb --only-show-errors"
 
 # install the ContosoISD package for an example the user can walk through
 echo "--> Setting up the example OEA package."
--- packages/ContosoISD/setup.sh
+++ packages/ContosoISD/setup.sh
@@ -12,11 +12,8 @@ synapse_workspace=$1
 this_file_path=$(dirname $(realpath $0))
 
 echo "--> Installing: $this_file_path, for synapse workspace: $synapse_workspace"
-module_path="$this_file_path/../../modules"
 
 # Import the notebooks
 echo "--> Importing synapse notebooks for ContosoISD example..."
 eval "az synapse notebook import --workspace-name $synapse_workspace --name ContosoISD_example --file @$this_file_path/notebook/ContosoISD_example.ipynb --only-show-errors"
 eval "az synapse notebook import --workspace-name $synapse_workspace --name example_modules_py --file @$this_file_path/notebook/example_modules_py.ipynb --only-show-errors"
-
-
